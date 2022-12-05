9feaa106bdab99ea7118b012e6da3b3f30e74c00
gary <110794817+vvrestler@users.noreply.github.com>
2022-12-05 17:54:56 -0500

    Create mathematic operations

---
+++ javascript.js
@@ -0,0 +1,43 @@
+function add(addend1, addend2) {
+    return addend1 + addend2;
+}
+
+function subtract(minuend, subtrahend) {
+    return minuend - subtrahend;
+}
+
+function multiply(multiplicand, multiplier) {
+    return multiplicand * multiplier;
+}
+
+function divide(dividend, divisor) {
+    return dividend / divisor;
+}
+
+function power(base, power) {
+    return base ** power;
+}
+
+function operate(num1, operator, num2) {
+    let result = 0;
+    switch (operator) {
+        case '+':
+            result = add(num1, num2);
+            break;
+        case '-':
+            result = subtract(num1, num2);
+            break;
+        case '*':
+            result = multiply(num1, num2);
+            break;
+        case '/':
+            result = divide(num1, num2);
+            break;
+        case'^':
+            result = power(num1, num2);
+            break;
+        default:
+            console.log('operate() switch statement error');
+    }
+    return result;
+}
