c9174d2f639f675076290441fbfbedf772fc11d2
gary <110794817+vvrestler@users.noreply.github.com>
2022-12-07 15:54:58 -0500

    Add number functionality
    -Add event listeners for every button
    -Add function to update display when equals key is presssed

--- javascript.js
+++ javascript.js
@@ -18,6 +18,7 @@ function power(base, power) {
     return base ** power;
 }
 
+
 function operate(num1, operator, num2) {
     let result = 0;
     switch (operator) {
@@ -41,3 +42,109 @@ function operate(num1, operator, num2) {
     }
     return Math.round(result * 100) / 100;
 }
+
+
+let displayValue = "";
+function populateDisplay () {
+    display.textContent = displayValue;
+};
+
+const clearKey = document.getElementById('clear');
+clearKey.addEventListener('click', () => {
+    displayValue = "";
+    display.textContent = "";
+});
+
+const equalsKey = document.getElementById('equals');
+equalsKey.addEventListener('click', () => {
+    operate(/* add values here */);
+    populateDisplay();
+});
+
+const additionKey = document.getElementById('addition');
+additionKey.addEventListener('click', () => {});
+
+const subtractKey = document.getElementById('subtraction');
+subtractKey.addEventListener('click', () => {});
+
+const multiplicationKey = document.getElementById('multiplication');
+multiplicationKey.addEventListener('click', () => {});
+
+const divisionKey = document.getElementById('division');
+divisionKey.addEventListener('click', () => {});
+
+const powerKey = document.getElementById('power');
+powerKey.addEventListener('click', () => {});
+
+const display = document.getElementById('display');
+
+
+const oneKey = document.getElementById('number-1');
+oneKey.addEventListener('click' , () => {
+    displayValue = displayValue + "1";
+});
+
+const twoKey = document.getElementById('number-2');
+twoKey.addEventListener('click' , () => {
+    displayValue = displayValue + "2";
+});
+
+const threeKey = document.getElementById('number-3');
+threeKey.addEventListener('click' , () => {
+    displayValue = displayValue + "3";
+});
+
+const fourKey = document.getElementById('number-4');
+fourKey.addEventListener('click' , () => {
+    displayValue = displayValue + "4";
+});
+
+const fiveKey = document.getElementById('number-5');
+fiveKey.addEventListener('click' , () => {
+    displayValue = displayValue + "5";
+});
+
+const sixKey = document.getElementById('number-6');
+sixKey.addEventListener('click' , () => {
+    displayValue = displayValue + "6";
+});
+
+const sevenKey = document.getElementById('number-7');
+sevenKey.addEventListener('click' , () => {
+    displayValue = displayValue + "7";
+});
+
+const eightKey = document.getElementById('number-8');
+eightKey.addEventListener('click' , () => {
+    displayValue = displayValue + "8";
+});
+
+const nineKey = document.getElementById('number-9');
+nineKey.addEventListener('click' , () => {
+    displayValue = displayValue + "9";
+});
+
+const zeroKey = document.getElementById('number-0');
+zeroKey.addEventListener('click' , () => {
+    if(displayValue == "") return;   //to avoid numbers like "00546"
+    displayValue = displayValue + "0";
+});
+//display pseudocode
+/*
+each number press concats to a display string
+
+call populateDisplay()
+*/
+
+//psuedocode
+/*
+add event listeners for button presses
+store a number as a string in a variable
+    add to the string for subsequent number presses
+store operator upon button press
+    make sure that only the most recent operator is stored
+store a second number as another variable
+listen for the equals sign
+use the two numbers and the operator in the opearate() function
+display the result in the display div
+*/
